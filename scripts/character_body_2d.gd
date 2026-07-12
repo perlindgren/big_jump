b4f0bf6a983b521f69aae1c2cd676b3f815914b1
Per Lindgren <per.lindgren@ltu.se>
2026-07-12 23:30:28 +0200

    player movement

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -1000.0
 
+
+@export var jump_velocity: float = 1000.0
+@export var acceleration: float = 1000.0
+@export var air_acceleration: float = 200.0
+@export var friction: float = 600.0
+@export var air_friction: float = 1.0
+@export var max_speed: float = 1000.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +16,26 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_released("ui_accept") and is_on_floor():
+		velocity.y = -jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
-		velocity.x = direction * SPEED
+		if is_on_floor():
+			velocity.x += direction * acceleration * delta
+			print("floor angle", get_floor_angle())
+		else:
+			velocity.x += direction * air_acceleration * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		# Apply friction if there is no input
+		if velocity.length() > (friction * delta):
+			velocity -= velocity.normalized() * friction * delta
+		else:
+			velocity = Vector2.ZERO
+		
+	### Cap speed
+	velocity = velocity.limit_length(max_speed)
 	move_and_slide()
